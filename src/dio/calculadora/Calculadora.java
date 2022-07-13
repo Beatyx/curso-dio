3a50d296519964407baf4019bcf5a5c74be423c8
Beatyx <anaaizaltino@gmail.com>
2022-07-13 18:51:10 -0300

    Criando uma calculadora para testar o debug

---
+++ src/dio/calculadora/Calculadora.java
@@ -0,0 +1,44 @@
+package dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b= scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		double divisao = divisao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		
+		System.out.println("Soma: " + soma);
+		System.out.println("Subtração: " + subtracao);
+		System.out.println("Divisão: " + divisao);
+		System.out.println("Multiplicação: " + multiplicacao);
+	
+	}
+	
+	
+	public static int soma(int a, int b ) {
+		return a + b;
+	}
+	public static int subtracao(int a, int b ) {
+		return a - b;
+	}
+	public static double divisao(int a, int b ) {
+		return a / b;
+	}
+	public static int multiplicacao(int a, int b ) {
+		return a * b;
+	}
+	
+	
+	
+}
