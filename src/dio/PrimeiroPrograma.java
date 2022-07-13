ddb96e0bca82f71980e1aec459c0b2dcbfe26f34
Beatyx <anaaizaltino@gmail.com>
2022-07-13 17:44:09 -0300

    fisrt commit

---
+++ src/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		
+		System.out.println("Hello World!" + (a+b));
+
+	}
+
+}
