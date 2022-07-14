a67ea876de4eff72269b063f5f19eda842cbdba6
Beatyx <anaaizaltino@gmail.com>
2022-07-13 23:28:26 -0300

    Inserindo o projeto no intelliJ

---
+++ primeiroprojeto_intellij/untitled/src/dio/Primeiro_Programa.java
@@ -0,0 +1,13 @@
+package dio;
+import dio.model.Gato;
+
+public class Primeiro_Programa {
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 2;
+        System.out.println("Hello World!" + (a+b));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
